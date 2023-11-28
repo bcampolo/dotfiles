7ce6539c1494e306571661bb8e2c52d462f2d2ef
bcampolo <bjcampolo@gmail.com>
2023-11-27 19:27:15 -0500

    Fix colorscheme overrides and turn off lazy notifications

--- .config/nvim/lua/core/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd.colorscheme "kanagawa"
--- override diff highlight colors
-vim.cmd('hi DiffAdd guifg=#00FF00 guibg=#005500')
-vim.cmd('hi DiffDelete guifg=#FF0000 guibg=#550000')
-vim.cmd('hi DiffChange guifg=#FFFFFF guibg=#000055')
-vim.cmd('hi DiffText guifg=#FFFF00 guibg=#550000')
-
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,5 +3,15 @@ return {
   'rebelot/kanagawa.nvim',
   lazy = false,
   priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme kanagawa]])
+    -- Colorscheme overrides
+    vim.cmd([[
+      autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
+      autocmd VimEnter * hi DiffDelete guifg=#FF0000 guibg=#550000
+      autocmd VimEnter * hi DiffChange guifg=#CCCCCC guibg=#555555
+      autocmd VimEnter * hi DiffText guifg=#00FF00 guibg=#005500
+    ]])
+  end
 }
 
