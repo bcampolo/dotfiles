bf4bb09f8541e4f94515c77c0ea222df2a0d3b43
bcampolo <bjcampolo@gmail.com>
2024-01-10 18:43:55 -0500

    sync up between configs

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -16,7 +16,7 @@ local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-    'java', 
+    'java',
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,4 +1,6 @@
+-- Highlight hex values with its actual colors
 return {
+  -- https://github.com/norcalli/nvim-colorizer.lua
   'norcalli/nvim-colorizer.lua',
   event = 'VeryLazy',
   opts = {},
